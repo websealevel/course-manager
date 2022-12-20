6879f730f6221f50ead6ab4a91df6a6f9570fa77
websealevel <contact@websealevel.com>
2022-12-20 08:32:38 +0100

    refactor Course en AbstractNode

--- src/Models/Course.php
+++ src/Models/Course.php
@@ -4,14 +4,13 @@ namespace Wsl\CourseManager\Models;
 
 
 use Wsl\CourseManager\Models\Module;
-use Wsl\CourseManager\Services\FileManager;
 use Wsl\CourseManager\Services\DefaultContent;
 
 /**
  * Une classe qui décrit un cours. Un cours est éventuellement placé dans un vendor (établissement, organisme, etc.)
  * et est défini par un nom. Un cours possède des métadonnées (niveau des apprenant·es, mots-clefs, etc.)
  */
-class Course
+class Course extends AbstractNode
 {
     readonly public array $modules;
 
@@ -24,6 +23,34 @@ class Course
     ) {
 
         $this->modules = array(new Module($this->absPathRootDirectory, 0, 'presentation'));
+    }
+
+    public function getAbsPathOfRootDirectory(): string
+    {
+        return $this->absPathRootDirectory;
+    }
+
+    public function getDefaultDirectories(): array
+    {
+        return array(
+            new Directory('bibliographie'),
+        );
+    }
+
+    public function getDefaultFiles(): array
+    {
+
+        $metaDataContent = <<< INI
+        name={$this->name}
+        level={$this->level}
+        keywords={$this->keywords}
+        INI;
+
+        return array(
+            new File('README.md', DefaultContent::readmeContent($this->name)),
+            new File('index.html', DefaultContent::indexHtmlContent($this->name)),
+            new File('.metadata', $metaDataContent)
+        );
     }
 
     /**
@@ -51,50 +78,5 @@ class Course
     public function absPath(): string
     {
         return sprintf("%s%s", $this->absPathRootDirectory, $this->path());
-    }
-
-    /**
-     * Initialise le répertoire d'un cours nouvellement créee (création de dossiers et de fichiers par défaut)
-     * @return bool
-     */
-    public function create(): bool
-    {
-
-        FileManager::createDirectory($this->absPath(), 'cours');
-
-        $defaultDirs = array(
-            'bibliographie',
-        );
-
-        $metaDataContent = <<< INI
-        name={$this->name}
-        level={$this->level}
-        keywords={$this->keywords}
-        INI;
-
-        $defaultFiles = array(
-            'README.md' => DefaultContent::readmeContent($this->name),
-            'index.html' => DefaultContent::indexHtmlContent($this->name),
-            '.metadata' => $metaDataContent
-        );
-
-        //Création des sous dossiers par défaut
-        foreach ($defaultDirs as $dir) {
-            $absPath = sprintf("%s/%s", $this->absPath(), $dir);
-            FileManager::createDirectory($absPath);
-        }
-
-        //Création des fichiers par défaut
-        foreach ($defaultFiles as $file => $content) {
-            $absPath =  sprintf("%s/%s", $this->absPath(), $file);
-            FileManager::createFile($absPath, $content);
-        }
-
-        //Préparation des repertoires des modules
-        foreach ($this->modules as $module) {
-            // $module->prepareModuleDirectory($this->path(), $this->level);
-        }
-
-        return true;
     }
 }
