6a19af66c7f2cb04e145dbf5406717c5e7b69de8
websealevel <contact@websealevel.com>
2022-12-24 07:20:45 +0100

    contenu par defaut fichier index.html (listing fichiers) a cree APRES creation du projet, sur le hook afterbuilding

--- src/Models/Project.php
+++ src/Models/Project.php
@@ -6,6 +6,7 @@ namespace Wsl\CourseManager\Models;
 use Wsl\CourseManager\Models\AbstractNode;
 use Wsl\CourseManager\Services\DefaultContent;
 use Wsl\CourseManager\Config;
+use Wsl\CourseManager\Services\FileManager;
 
 /**
  * Class représentant un projet de gestion de cours
@@ -38,8 +39,15 @@ class Project extends AbstractNode
     public function getDefaultFiles(): array
     {
         return array(
-            new File('index.html', DefaultContent::indexHtmlContent('Liste des cours', $this->absPath)),
+            new File('index.html', ''),
             new File('config.ini', Config::configIniContent())
         );
+    }
+
+    public function hookAfterBuilding(): void
+    {
+        //Mettre le contenu par défaut du fichier index.html (listing des fichiers)
+        $tree = FileManager::treeStructureUnderPath($this->absPath);
+        var_dump($tree);
     }
 }
