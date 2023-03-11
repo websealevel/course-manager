34e54b9ffd59f2eea7b3ba2f870a6407010af266
p. schuhmacher <contact@pschuhmacher.com>
2023-03-11 06:56:15 +0100

    ajoute une methode pour detecter si un repertoire contient des repertoires

--- src/Models/Directory.php
+++ src/Models/Directory.php
@@ -8,21 +8,17 @@ namespace Wsl\CourseManager\Models;
 class Directory
 {
     public function __construct(
-
         public readonly string $name,
-
         /**
          * Liste des dossiers qui doivent être présents
          * @var Directory[]
          */
         public readonly array $directories = array(),
-
         /**
          * Liste des fichiers qui doivent être présents
          * @var File[]
          */
         public readonly array $files = array(),
-
         public readonly string $description = ''
     ) {
     }
@@ -34,5 +30,14 @@ class Directory
     public function hasFiles(): bool
     {
         return 0 !== count($this->files);
+    }
+
+    /**
+     * Retourne vrai si le repertoire contient
+     * des repertoires, faux sinon.
+     */
+    public function hasDirs(): bool
+    {
+        return 0 !== count($this->directories);
     }
 }
